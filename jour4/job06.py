12a4997608cf24463e489f0dbe6da4c3d61cf838
galeb-madjinda <115154388+galeb-madjinda@users.noreply.github.com>
2023-01-12 10:30:55 +0100

    Update job06.py

--- jour4/job06.py
+++ jour4/job06.py
@@ -1,12 +1,9 @@
 def echange_premiere_derniere(liste):
-  # Vérifie que la liste contient au moins deux éléments
   if len(liste) < 2:
     return
 
-  # Échange les valeurs de la première et de la dernière case
   liste[0], liste[-1] = liste[-1], liste[0]
 
-# Exemple d'utilisation
 ma_liste = [1, 2, 3, 4, 5]
 echange_premiere_derniere(ma_liste)
 print(ma_liste)
