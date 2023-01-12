f0446591094b703fa071b8c2bd0e9132fb90e226
galeb-madjinda <115154388+galeb-madjinda@users.noreply.github.com>
2023-01-12 10:40:29 +0100

    Update job11.py

--- jour4/job11.py
+++ jour4/job11.py
@@ -1,10 +1,7 @@
 def augmente_liste(liste):
-  # Parcours tous les éléments de la liste
   for i, element in enumerate(liste):
-    # Augmente la valeur de l'élément de 1
     liste[i] = element + 1
 
-# Exemple d'utilisation
 L = [7, 11, 42, 39, 2]
 augmente_liste(L)
 print(L)
